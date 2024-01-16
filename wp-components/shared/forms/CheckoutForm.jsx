34fdaf7bf6487924ff4a384d4d8e1ec2f74db5fa
necip yolcu <ncpyolcu@gmail.com>
2024-01-16 11:20:59 +0100

    cors try - 4

--- wp-components/shared/forms/CheckoutForm.jsx
+++ wp-components/shared/forms/CheckoutForm.jsx
@@ -62,7 +62,7 @@ export default function CheckoutForm({ email }) {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000/account/checkout",
+        return_url: "http://del-martf.vercel.app/account/checkout",
         receipt_email: email,
       },
       //redirect: "if_required",
