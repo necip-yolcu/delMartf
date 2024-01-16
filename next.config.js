428098a7863c53eadbf1c144e80ac210e8938872
necip yolcu <ncpyolcu@gmail.com>
2024-01-16 11:46:50 +0100

    cors try - 8

--- next.config.js
+++ next.config.js
@@ -46,10 +46,10 @@ module.exports = {
         return [
         {
             // matching all API routes
-            source: "/api/:path*",
+            source: "https://clone.trendmall.nl/:path*",
             headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Origin", value: "https://del-martf.vercel.app" },
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
             ]
